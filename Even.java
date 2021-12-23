fe083b2158ba405e80efc0c1e820c558d534c462
AkshatRana <akshat.xworkz@gmail.com>
2021-12-23 21:31:50 +0530

    even, odd and series files is uploaded

---
+++ Even.java
@@ -0,0 +1,11 @@
+class Even{
+		public static void main(String[] args){
+			int evenORnot = 10;
+			if(evenORnot %2 == 0){
+			 System.out.println("EVEN");
+			 }else{ 
+				System.out.println("ODD");
+			 }
+	}
+
+}
